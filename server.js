35a43d37411bc6a08d496dab6373e022e5bb4ec6
Kentiba <kentiba93@gmail.com>
2019-03-15 20:52:59 +0100

    prepared for deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 const app = express();
 
@@ -22,10 +23,7 @@ const db = require('./config/keys').mongoURI;
 
 //connect to mongoDB
 mongoose
-    .connect(
-        db,
-        {useNewUrlParser: true},
-    )
+    .connect(db, {useNewUrlParser: true})
     .then(() => console.log('mongoDB is connect'))
     .catch(err => console.log(err));
 
@@ -39,5 +37,15 @@ require('./config/passport')(passport);
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
+
+//Server static assests if in production
+if (process.env.NODE_ENV === 'production') {
+    //Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 app.listen(port, () => console.log(`Server is running on port${port}`));
